58fc88870f4a88bd5642bcc9f9a6a2686001f9d3
Roi Vazan <roi.vazan@kzencorp.com>
2022-07-18 13:57:35 +0300

    Updates

---
+++ precache-manifest.7dad559b66e31b8c7bbe7f769363c4b1.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "2af037172ad4c5556722",
+    "url": "/static/js/main.2af03717.chunk.js"
+  },
+  {
+    "revision": "50493e61136e5ba3707c",
+    "url": "/static/js/1.50493e61.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "e1cb8d21bffbcc2a8c7bfcf95bad12ff",
+    "url": "/static/media/walletconnect-banner.e1cb8d21.png"
+  },
+  {
+    "revision": "906078994f7221a25b900f47425b3ecc",
+    "url": "/static/media/erc20.90607899.svg"
+  },
+  {
+    "revision": "42bca7317690c3f5e9ecd8db387344df",
+    "url": "/static/media/eth.42bca731.svg"
+  },
+  {
+    "revision": "e37299a879ceb5a23000d8a2bb10bc46",
+    "url": "/static/media/xdai.e37299a8.png"
+  },
+  {
+    "revision": "1163d84fed933c29d8cf5dfe1d91b0f6",
+    "url": "/index.html"
+  }
+];
